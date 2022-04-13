e64ac3814dcf57935f8b2cfe8434b78c5ee4934b
rightanglewrong <bennyyang996648@hotmail.com>
2022-04-13 16:17:38 -0400

    Version 1.0 -ready for hand in

--- script.js
+++ script.js
@@ -9,75 +9,54 @@ const playerTwoEl = document.querySelector("#player2");
 const playerTwoScore = document.querySelector("#p2");
 const cellsEl = document.querySelectorAll(".box");
 
+let playerOneScoreNum = 0;
+let playerTwoScoreNum = 0;
+
 const players = [1, -1];
 let turn = -1;
 let turnCounter;
-// REORGANIZE
+let quoteList = [
+  "Put the bunny back in the box.",
+  "Sorry boss, but there's only two men I trust. One of them's me. The other's not you.", 
+  "Hey! My mama lives in a trailer!", 
+  "Somehow they managed to get every creep and freak in the universe on this one plane.",
+  "Define irony. Bunch of idiots dancing on a plane to a song made famous by a band that died in a plane crash.",
+  "Make a move and the bunny gets it."
+]
+
+// Winning combinations 
 const winningCombos = [
-  // Rows
   [0, 1, 2, 3],
   [1, 2, 3, 4],
   [5, 4, 3, 2],
-  [41, 40, 39, 38],
-  [7, 8, 9, 10],
-  [34, 33, 32, 31],
-  [14, 15, 16, 17],
-  [27, 26, 25, 24],
-  [21, 22, 23, 24],
-  [20, 19, 18, 17],
-  [28, 29, 30, 31],
-  [13, 12, 11, 10],
-  [35, 36, 37, 38],
   [6, 5, 4, 3],
-  [0, 7, 14, 21],
-  [41, 34, 27, 20],
-  [1, 8, 15, 22],
-  [40, 33, 26, 19],
-  [2, 9, 16, 23],
-  [39, 32, 25, 18],
-  [3, 10, 17, 24],
-  [38, 31, 24, 17],
-  [4, 11, 18, 25],
-  [37, 30, 23, 16],
-  [5, 12, 19, 26],
-  [36, 29, 22, 15],
-  [6, 13, 20, 27],
-  [35, 28, 21, 14],
-  [0, 8, 16, 24],
-  [41, 33, 25, 17],
-  [7, 15, 23, 31],
-  [34, 26, 18, 10],
-  [14, 22, 30, 38],
-  [27, 19, 11, 3],
-  [35, 29, 23, 17],
-  [6, 12, 18, 24],
-  [28, 22, 16, 10],
-  [13, 19, 25, 31],
-  [21, 15, 9, 3],
-  [20, 26, 32, 38],
-  [36, 30, 24, 18],
-  [5, 11, 17, 23],
-  [37, 31, 25, 19],
-  [4, 10, 16, 22],
-  [2, 10, 18, 26],
-  [39, 31, 23, 15],
-  [1, 9, 17, 25],
-  [40, 32, 24, 16],
-  [9, 17, 25, 33],
-  [8, 16, 24, 32],
-  [11, 17, 23, 29],
-  [12, 18, 24, 30],
-  
+  [7, 8, 9, 10],
   [8, 9, 10, 11],
   [12, 11, 10, 9],
+  [13, 12, 11, 10],
+  [14, 15, 16, 17],
   [15, 16, 17, 18],
   [19, 18, 17, 16],
+  [20, 19, 18, 17],
   [22, 23, 24, 25],
   [26, 25, 24, 23],
-  [29, 30, 31, 32],
+  [27, 26, 25, 24],
+  [21, 22, 23, 24],
+  [28, 29, 30, 31],
   [33, 32, 31, 30],
+  [29, 30, 31, 32],
+  [31, 32, 33, 34],
+  [35, 36, 37, 38],
   [36, 37, 38, 39],
   [40, 39, 38, 37],
+  [38, 39, 40, 41],
+  [0, 7, 14, 21],
+  [1, 8, 15, 22],
+  [2, 9, 16, 23],
+  [3, 10, 17, 24],
+  [4, 11, 18, 25],
+  [5, 12, 19, 26],
+  [6, 13, 20, 27],
   [7, 14, 21, 28],
   [8, 15, 22, 29],
   [9, 16, 23, 30],
@@ -85,6 +64,37 @@ const winningCombos = [
   [11, 18, 25, 32],
   [12, 19, 26, 33],
   [13, 20, 27, 34],
+  [35, 28, 21, 14],
+  [36, 29, 22, 15],
+  [37, 30, 23, 16],
+  [38, 31, 24, 17],
+  [39, 32, 25, 18],
+  [40, 33, 26, 19],
+  [41, 34, 27, 20],
+  [0, 8, 16, 24],
+  [1, 9, 17, 25],
+  [2, 10, 18, 26],
+  [21, 15, 9, 3],
+  [27, 19, 11, 3],
+  [4, 10, 16, 22],
+  [5, 11, 17, 23],
+  [6, 12, 18, 24],
+  [7, 15, 23, 31],
+  [8, 16, 24, 32],
+  [9, 17, 25, 33],
+  [34, 26, 18, 10],
+  [28, 22, 16, 10],
+  [11, 17, 23, 29],
+  [12, 18, 24, 30],
+  [13, 19, 25, 31],
+  [14, 22, 30, 38],
+  [39, 31, 23, 15],
+  [40, 32, 24, 16],
+  [35, 29, 23, 17],
+  [41, 33, 25, 17],
+  [36, 30, 24, 18],
+  [37, 31, 25, 19],
+  [20, 26, 32, 38],
 ];
 
 statusEl.textContent =
@@ -113,7 +123,6 @@ function playGame() {
   }
   for (let i = 0; i < cellsEl.length; i++) {
     cellsEl[i].addEventListener("click", function () {
-      console.log(i)
       if (
         cellsEl[i].classList.contains("end") &&
         !cellsEl[i].classList.contains("taken")
@@ -125,7 +134,7 @@ function playGame() {
       ) {
         turnState(i);
       } else {
-        statusEl.textContent = "Can't go here";
+        statusEl.textContent = "Nope, try again...";
         return;
       }
       checkBoard();
@@ -169,30 +178,38 @@ function checkBoard() {
       square3.classList.contains("red") &&
       square4.classList.contains("red")
     ) {
+      playerOneScoreNum += 1;
       statusEl.textContent = "Player One Wins! Resetting board!";
-      playerOneScore.textContent += "*";
+      playerOneScore.textContent = "Wins: " +  playerOneScoreNum;
       turn = 1;
-      setTimeout(resetBoard, 3000);
+      setTimeout(resetBoard, 1400);
     } else if (
       square1.classList.contains("green") &&
       square2.classList.contains("green") &&
       square3.classList.contains("green") &&
       square4.classList.contains("green")
     ) {
+      playerTwoScoreNum += 1;
       statusEl.textContent = "Player Two Wins! Resetting board!";
-      playerTwoScore.textContent += "*";
+      playerTwoScore.textContent = "Wins: " + playerTwoScoreNum ;
       turn = -1;
-      setTimeout(resetBoard, 3000);
-    }
+      setTimeout(resetBoard, 1400);
+      }
   }
 }
+  
+
 
 // Game reset
 function resetGame() {
-  statusEl.textContent = "Put the bunny back in the box.";
+  let quoteNumber = Math.floor(Math.random() * (quoteList.length))
+  statusEl.textContent = quoteList[quoteNumber];
+  // "Put the bunny back in the box.";
   resetBoard()
   playerOneScore.textContent = "";
   playerTwoScore.textContent = "";
+  playerOneScoreNum = 0
+  playerTwoScoreNum = 0
   turn = -1;
 }
 function resetBoard() {
